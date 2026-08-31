0985dbbe6a156ea096938280975933318a176cf6
Sarthak Mandal <sarthakmandal17112002@gmail.com>
2026-08-31 08:40:26 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             // Display the prompt
